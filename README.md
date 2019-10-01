694d3e14076ee18b8229a28fd6917556a7236e3e
stormi186 <jasna.misimovic@gmail.com>
2019-10-01 11:40:31 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Phonebook
+
+## User Stories
+
+### 3.1: Phonebook backend step1
+
+User can see a list of phonebook entries from the address http://localhost:3001/api/persons
+
+### 3.2: Phonebook backend step2
+
+User can see info for the phonebook at address http://localhost:3001/info
+
+### 3.3: Phonebook backend step3
+
+User can see the information for a single phonebook entry
+
+### 3.4: Phonebook backend step4
+
+User can delete a person
+
+### 3.5: Phonebook backend step5
+
+User can add a person
+
+### 3.6: Phonebook backend step6
+
+User can't add a person without a name, number or an existing person
+
+### 3.7: Phonebook backend step7
+
+User can see log messages using morgan middleware
+
+### 3.8*: Phonebook backend step8
+
+User can see in console the data sent to server
+
+### 3.9 phonebook backend step9
+
+User can see phonebook entries because backend works with the frontend
---
+++ index.js
@@ -0,0 +1,128 @@
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+
+app.use(cors())
+
+app.use(bodyParser.json())
+
+morgan.token('body', (req, res) => { return JSON.stringify(req.body) })
+
+var loggerFormat = ':method :url :status :res[content-length] - :response-time ms :body'
+
+app.use(morgan(loggerFormat, {
+  skip: (req, res) => {
+      return res.statusCode < 400
+  },
+  stream: process.stderr
+}))
+
+app.use(morgan(loggerFormat, {
+  skip: (req, res) => {
+      return res.statusCode >= 400
+  },
+  stream: process.stdout
+}))
+
+let persons = [
+  { 
+    "name": "Arto Hellas", 
+    "number": "040-123456",
+    "id": 1
+  },
+  { 
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  { 
+    "name": "Dan Abramov", 
+    "number": "12-43-234345",
+    "id": 3
+  },
+  { 
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122",
+    "id": 4
+  }
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Phonebook App</h1>')
+})
+
+const getTotal = () => { return persons.length }
+
+app.get('/info', (req, res) => {
+  const date = new Date()
+  const total = getTotal()
+  res.send(`Phonebook has info for ${total} people ` +'<br/>' + `${date}`)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+const generateId = () => { return Math.floor(Math.random() * 1000) }
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  console.log(body)
+
+  if (!body.name) {
+    return res.status(400).json({ 
+      error: 'name is missing' 
+    })
+  }
+
+  if (!body.number) {
+    return res.status(400).json({ 
+      error: 'number is missing' 
+    })
+  }
+
+  const existingPerson = persons.find(o => o.name === body.name)
+
+  if(typeof existingPerson !== 'undefined') {
+    return res.status(400).json({ 
+      error: 'that person exists in the phonebook' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number || 0,
+    id: generateId()
+  }
+
+  persons = persons.concat(person)
+  console.log(persons)
+  res.json(person)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
